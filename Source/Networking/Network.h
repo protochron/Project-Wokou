c5ff3a695f3b069cd2cab2c89400a74430bb0394
Codiferous <codymiller@vandals.uidaho.edu>
2011-04-17 23:10:30 -0700

    Adds the Singleton interface for the Network class.

--- Source/Networking/Network.h
+++ Source/Networking/Network.h
@@ -22,11 +22,38 @@
 #ifndef _PIRATE_NETWORK_H_
 #define _PIRATE_NETWORK_H_
 
-#include <boost/detail>
+#include <boost/shared_ptr.hpp>
+//#include <boost/asio.hpp>
 
 
+/** This serves as the interface to all network-related actions in the game. It
+ * is a Singleton, so only one instance can be created in the game. This means
+ * that the network connection can be accessed from anywhere in the code
+ * without problems (barring anything terrible about threading).
+ */
+
+class Network {
+public:
+  
+  //! Retrieves the networking instance or creates it if it hasn't been.
+  static Network* instance();
+  
+  
+  
+  
+protected:
+  
+  // Do not allow creation of this class. It's a Singleton, baby!
+  Network();
+  
+private:
+  
+  static boost::shared_ptr<Network> instance_;
+  
+};
 
 
+boost::shared_ptr<Network> Network::instance_;
 
 
 
