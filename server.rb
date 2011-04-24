721e4e3744b6dc65d3728fb445c64867d72c1f0f
Codiferous <codymiller@vandals.uidaho.edu>
2011-04-24 02:31:56 -0700

    Adds plant Blender models and a Ruby server.
    The plant models were found online under CC or free licenses. The server is just a simple echo server used to test the network part of the client.

---
+++ server.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'eventmachine'
+
+module EchoServer
+  def post_init
+    puts "Client connected!"
+  end
+  
+  def receive_data(data)
+    puts data
+    send_data(data + "\n")
+  end
+end
+
+EventMachine::run do
+  host = '0.0.0.0'
+  post = 8080
+  EventMachine::start_server host, post, EchoServer
+  puts "Started EchoServer on #{host}:#{post}..."
+
+end
+
+
